ad639af8d533d9de494fd4441bce5bedb71d8fae
nasir-buddy <syedali172000@gmail.com>
2022-09-15 20:30:41 +0530

    fix fibonacci dynamically

--- Dynamic_Programming/Fibonacci.java
+++ Dynamic_Programming/Fibonacci.java
@@ -11,26 +11,26 @@ public class Fibonacci {
     }
 
     // Dynamically
-    // public static int fibM(int n) {
-    //     int storage[] = new int[n + 1];
-    //     for (int i = 0; i <= n; i++) {
-    //         storage[i] = -1;
-    //     }
-    //     return fibM(n, storage);
-    // }
+    public static int fibM(int n) {
+        int storage[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            storage[i] = -1;
+        }
+        return fibM(n, storage);
+    }
 
-    // public static int fibM(int n, int storage[]) {
-    //     if (n == 0 || n == 1) {
-    //         storage[n] = n;
-    //         return storage[n];
-    //     }
-    //     if (storage[n] != -1) {
-    //         return storage[n];
-    //     }
+    public static int fibM(int n, int storage[]) {
+        if (n == 0 || n == 1) {
+            storage[n] = n;
+            return storage[n];
+        }
+        if (storage[n] != -1) {
+            return storage[n];
+        }
 
-    //     storage[n] = fibM(n - 1, storage) + fibM(n - 2, storage);
-    //     return storage[n];
-    // }
+        storage[n] = fibM(n - 1, storage) + fibM(n - 2, storage);
+        return storage[n];
+    }
 
     public static void main(String[] args) {
         // for calculating time
@@ -38,7 +38,7 @@ public class Fibonacci {
         // main program begins from here
         
         int n = 43;
-        // System.out.println("Answer = " + fibM(n));
+        System.out.println("Answer = " + fibM(n));
         System.out.println("Answer = " + fib(n));
         // for ending time
 
