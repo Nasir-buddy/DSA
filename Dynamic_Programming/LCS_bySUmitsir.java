8179235f54ca585737e4a0154e7d002946c2c1c6
nasir-buddy <syedali172000@gmail.com>
2022-09-25 16:12:06 +0530

    LCS by sumit sir dynamic approach

---
+++ Dynamic_Programming/LCS_bySUmitsir.java
@@ -0,0 +1,24 @@
+package DSA.Dynamic_Programming;
+import java.util.Scanner;
+public class LCS_bySUmitsir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        int dp[][] = new int [s1.length() + 1][s2.length() + 1];
+        for(int i = dp.length - 2; i >= 0; i--){
+            for(int j = dp[0].length - 2; j >= 0; j--){
+                char c1 = s1.charAt(i);
+                char c2 = s2.charAt(j);
+
+                if(c1 == c2){
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j +1]);
+                }
+            }
+        }
+        System.out.println(dp[0][0]);
+    }   
+}
