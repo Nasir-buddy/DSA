c8deab392a6701e1c8b9a06ce1b4fcf1ace9ae73
nasir-buddy <syedali172000@gmail.com>
2022-10-03 15:40:56 +0530

    Backtracking : Rat in a maze

---
+++ Backtracking/Rat_in_a_Maze.java
@@ -0,0 +1,51 @@
+package DSA.Backtracking;
+
+public class Rat_in_a_Maze {
+    public static boolean ratInAMaze(int maze[][]) {
+        int n = maze.length;
+        int path[][] = new int[n][n];
+
+        return solveMaze(maze, 0, 0, path);
+    }
+
+    public static boolean solveMaze(int maze[][], int i, int j, int path[][]) {
+        // actual recursive login and actual backtracking is implemented her
+
+        int n = maze.length;
+        // Check if i , j cell is valir or not
+        if (i < 0 || i >= n || j < 0 | j >= n || maze[i][j] == 0 || path[i][j] == 1) {
+            return false;
+        }
+        // Inclede the cell in current path
+        path[i][j] = 1;
+        // Destination cell
+        if (i == n - 1 && j == n - 1) {
+            return true;
+        }
+        // Explore further in all direction
+        // first direction is the first direction
+        //top
+        if (solveMaze(maze, i - 1, j, path)) {
+            return true;
+        }
+        // Right
+        if (solveMaze(maze, i, j + 1, path)) {
+            return true;
+        }
+        // Down
+        if (solveMaze(maze, i + 1, j, path)) {
+            return true;
+        }
+        // Left
+        if (solveMaze(maze, i, j - 1, path)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int maze[][] = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 1, 1 } };
+        boolean pathPossible = ratInAMaze(maze);
+        System.out.println(pathPossible);
+    }
+}
