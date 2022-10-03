21a8a95566374480ab7653d3d91bde1fde630fae
nasir-buddy <syedali172000@gmail.com>
2022-10-03 16:17:26 +0530

    Backtracking : make rat in a maze can print all possibble path

--- Backtracking/Rat_in_a_Maze.java
+++ Backtracking/Rat_in_a_Maze.java
@@ -3,13 +3,49 @@ package DSA.Backtracking;
 import java.util.Scanner;
 
 public class Rat_in_a_Maze {
-    public static boolean ratInAMaze(int maze[][]) {
+    public static void ratInAMaze(int maze[][]) {
         int n = maze.length;
         int path[][] = new int[n][n];
-
-        return solveMaze(maze, 0, 0, path);
+        printAllPath(maze, 0, 0, path);
+        // return solveMaze(maze, 0, 0, path);
     }
 
+    public static void printAllPath(int maze[][], int i, int j, int path[][]) {
+        // actual recursive login and actual backtracking is implemented her
+
+        int n = maze.length;
+        // Check if i , j cell is valir or not
+        if (i < 0 || i >= n || j < 0 | j >= n || maze[i][j] == 0 || path[i][j] == 1) {
+            return;
+        }
+        // Inclede the cell in current path
+        path[i][j] = 1;
+        // Destination cell
+        if (i == n - 1 && j == n - 1) {
+            System.out.println("Printing the path...........");
+            for(int r = 0; r < n; r ++){
+                for(int c = 0; c < n ; c++){
+                    System.out.print(path[r][c] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            // we reseting the path to back to 0 to check another path 
+            path[i][j] = 0; 
+            return;
+        }
+        // Explore further in all direction
+        // first direction is the first direction
+        //top
+        printAllPath(maze, i - 1, j, path);
+        // Right
+        printAllPath(maze, i, j + 1, path);
+        // Down
+        printAllPath(maze, i + 1, j, path);
+        // Left
+        printAllPath(maze, i, j - 1, path);
+        path[i][j] =  0;
+    }
     public static boolean solveMaze(int maze[][], int i, int j, int path[][]) {
         // actual recursive login and actual backtracking is implemented her
 
@@ -22,6 +58,13 @@ public class Rat_in_a_Maze {
         path[i][j] = 1;
         // Destination cell
         if (i == n - 1 && j == n - 1) {
+            System.out.println("Printing the path...........");
+            for(int r = 0; r < n; r ++){
+                for(int c = 0; c < n ; c++){
+                    System.out.print(path[r][c] + " ");
+                }
+                System.out.println();
+            }
             return true;
         }
         // Explore further in all direction
@@ -56,7 +99,8 @@ public class Rat_in_a_Maze {
                 maze[i][j] = sc.nextInt();
             }
         }
-        boolean pathPossible = ratInAMaze(maze);
-        System.out.println(pathPossible);
+        ratInAMaze(maze);
+        // boolean pathPossible = ratInAMaze(maze);
+        // System.out.println(pathPossible);
     }
 }
