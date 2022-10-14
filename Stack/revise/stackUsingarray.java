3827f4911e21dead4f29d2873a620b29f4b69311
nasir-buddy <syedali172000@gmail.com>
2022-10-14 16:22:53 +0530

    Make stack more easier to undetstand

--- Stack/revise/stackUsingarray.java
+++ Stack/revise/stackUsingarray.java
@@ -1,6 +1,6 @@
 package DSA.Stack.revise;
 
-
+import java.util.Scanner;
 import DSA.Stack.StackEmptyException;
 import DSA.Stack.StackFullException;
 
@@ -10,7 +10,7 @@ public class stackUsingarray {
 
    //constructor
    public stackUsingarray(){
-    data = new int[10];
+    data = new int[5];
     top = -1;
    }
 
@@ -24,6 +24,7 @@ public class stackUsingarray {
    }
 
    public int size(){
+      System.out.println(top + 1);
       return top + 1;
    }
 
@@ -32,17 +33,23 @@ public class stackUsingarray {
          StackEmptyException e = new StackEmptyException();
          throw e;
       }
+      System.out.println(data[top] + " is the top element of the stack..");
       return data[top];
    }
 
-   public void push(int elem) throws StackFullException{
+   public void push(Scanner sc) throws StackFullException{
       if(size() == data.length){
-         StackFullException e = new StackFullException();
-         throw e;                                                
+         System.out.println("Calling Double capacity function.. ");
+         doubleCapacity();
+         // StackFullException e = new StackFullException();
+         // throw e;
       }
+      System.out.print("Enter the element of the stack : ");
+      int element = sc.nextInt();
       top++;
-      data[top] = elem;
-   }
+      data[top] = element;
+      System.out.println(element + " Pushed Successfully!!");
+   } 
 
    public int pop() throws StackEmptyException{ 
       if(size() == 0){
@@ -51,14 +58,72 @@ public class stackUsingarray {
       }
       int temp = data[top];
       top--;
+      System.out.println("Stack poped is successfully done .... \n");
       return temp;
+     
    }
 
    public void doubleCapacity(){
       int temp[] = data;
       data = new int[2 * temp.length];
       for(int i = 0 ; i < top ; i++){
-         temp[i] = data[i];
+         data[i] = temp[i];
+      } 
+   }
+   public void display(){
+      System.out.println("Printing the stack......");
+      for(int i = top; i >= 0; i--){
+         System.out.println(data[i]);
+      }
+   }
+   public static void main(String[] args) throws StackEmptyException , StackFullException {
+      Scanner sc = new Scanner(System.in);
+      stackUsingarray stack = new stackUsingarray();
+      System.out.print("******Stack Using Array******");
+      System.out.println("\n-----------------------------");
+      int choice = 0; 
+      while(choice != 6){
+         System.out.println("Choose from the below option.");
+         System.out.println(" 1. Push \n 2. Pop \n 3. Display \n 4. Top \n 5. size \n 6. Exit");
+         System.out.println("Enter your choice : ");
+         choice = sc.nextInt();
+         switch(choice){
+            case 1:
+            {
+               stack.push(sc);
+               break;
+            }
+            case 2:
+            {
+               stack.pop();
+               break;
+            }
+            case 3:
+            {
+               stack.display();
+               break;
+            }
+            case 4: 
+            {
+               stack.top();
+               break;
+            }
+            case 5:
+            {
+               stack.size();
+               break;
+            }
+            case 6:
+            {
+               System.out.println("Exiting program Successfully!!!!!");
+               System.exit(0);   
+               break;
+            }
+            default : 
+            {
+               System.out.print("Please enter the valid choice.....");
+            }
+         }
       }
    }
 }
