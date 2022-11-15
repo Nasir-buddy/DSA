37b627eed9737684ef6a3849a9e71d0c478763ad
nasir-buddy <syedali172000@gmail.com>
2022-11-15 14:24:53 +0530

    Array: Bubble sort

---
+++ Array/Algo/Bubble_Sort.java
@@ -0,0 +1,37 @@
+package DSA.Array.Algo;
+
+import java.util.Scanner;
+
+public class Bubble_Sort {
+    public static void sort(int arr[]) {
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.print("Enter the element of the array : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        print(arr);
+    }
+}
